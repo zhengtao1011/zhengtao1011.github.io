365354f03acf9a30fb20d62ce77edcd9fd4c1d5b
zhengtao1011 <2944677034@qq.com>
2016-04-12 16:52:28 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "2944677034@qq.com"
+git config --global user.name "zhengtao1011"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push 
+start http://zhengtao1011.github.io
